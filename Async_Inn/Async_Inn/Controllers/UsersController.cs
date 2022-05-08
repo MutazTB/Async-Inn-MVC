6d4dff6b2d5b5ca7eeb2ae25d26966d7b3e39399
MutazTB <mutazaltbakhi900@gmail.com>
2022-05-08 05:11:13 +0300

    Identity

---
+++ Async_Inn/Async_Inn/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Async_Inn.Models.DTOs;
+using Async_Inn.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IUser _userService;
+
+        public UsersController(IUser service)
+        {
+            _userService = service;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO data)
+        {
+            var user = await _userService.Register(data, this.ModelState);
+
+            if (ModelState.IsValid)
+            {
+                return user;
+            }
+
+            return BadRequest(new ValidationProblemDetails(ModelState));
+        }
+
+        [HttpPost("Signin")]
+        public async Task<ActionResult<UserDTO>> Signin(LoginDTO data)
+        {
+            var user = await _userService.Authenticate(data.Username, data.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            return user;
+        }
+    }
+}
---
+++ Async_Inn/Async_Inn/Services/Implementation/IdentityUserService.cs
@@ -0,0 +1,71 @@
+﻿using Async_Inn.Models;
+using Async_Inn.Models.DTOs;
+using Async_Inn.Services.Interface;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Services
+{
+    public class IdentityUserService : IUser 
+    {
+        private UserManager<ApplicationUser> _userManager;
+
+        public IdentityUserService(UserManager<ApplicationUser> manager)
+        {
+            _userManager = manager;           
+        }
+
+        public async Task<UserDTO> Authenticate(string username, string password)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (await _userManager.CheckPasswordAsync(user, password))
+            {
+                return new UserDTO
+                {
+                    Id = user.Id,
+                    Username = user.UserName,                   
+                };
+            }
+            return null;
+        }
+
+        public async Task<UserDTO> Register(RegisterDTO data, ModelStateDictionary modelState)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = data.Username,
+                Email = data.Email,
+                PhoneNumber = data.PhoneNumber
+            };
+
+            var result = await _userManager.CreateAsync(user, data.Password);
+
+            if (result.Succeeded)
+            {                                
+                return new UserDTO
+                {
+                    Id = user.Id,
+                    Username = user.UserName,                    
+                };
+            }
+
+            foreach (var error in result.Errors)
+            {
+                var errorKey =
+                    error.Code.Contains("Password") ? nameof(data.Password) :
+                    error.Code.Contains("Email") ? nameof(data.Email) :
+                    error.Code.Contains("UserName") ? nameof(data.Username) :
+                    "";
+
+                modelState.AddModelError(errorKey, error.Description);
+            }
+
+            return null;
+        }
+    }
+}
